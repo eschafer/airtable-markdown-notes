a08400b9bd83ca958a6310f938bc7fea9ce8ca25
Elizabeth Schafer <elizabeth.schafer.wenk@gmail.com>
2019-08-17 14:57:59 -0400

    Update airtable.js

--- src/lambda/airtable.js
+++ src/lambda/airtable.js
@@ -7,14 +7,26 @@ exports.handler = async (event, context) => {
   const base = await Airtable.base(process.env.BASE_ID)
   const table = await base('test-table')
 
+  /* const [error, record] = await table.create(
+    JSON.parse(event.body),
+    (error, record) => [error, record]
+  )
+
+  if (err) {
+    return {
+      statusCode: error.statusCode || 500,
+      body: String(error),
+    }
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(record),
+  } */
+  
   const response = await table.create(
     JSON.parse(event.body),
     (error, record) => {
-      console.log('error', error)
-      console.log('record', record)
-      return new Promise((resolve, reject) => {
-        resolve({ error, record })
-      })
+      return { error, record }
     }
   )
 
