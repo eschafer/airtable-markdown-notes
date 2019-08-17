452bb4b5bbdbb67ddf343ab6288cd773060b39d0
Elizabeth Schafer <elizabeth.schafer.wenk@gmail.com>
2019-08-17 14:06:38 -0400

    Adding back an await

--- src/lambda/airtable.js
+++ src/lambda/airtable.js
@@ -7,7 +7,7 @@ exports.handler = async (event, context) => {
   const base = await Airtable.base(process.env.BASE_ID)
   const table = await base('test-table')
 
-  table.create(JSON.parse(event.body), (err, record) => {
+  await table.create(JSON.parse(event.body), (err, record) => {
     if (err) {
       return {
         statusCode: error.statusCode || 500,
