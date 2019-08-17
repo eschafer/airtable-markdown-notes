541d9c73ef1484ca006a147aad88f4bf15043067
Elizabeth Schafer <elizabeth.schafer.wenk@gmail.com>
2019-08-17 14:13:42 -0400

    Reverting and testing...

--- src/lambda/airtable.js
+++ src/lambda/airtable.js
@@ -7,16 +7,19 @@ exports.handler = async (event, context) => {
   const base = await Airtable.base(process.env.BASE_ID)
   const table = await base('test-table')
 
-  await table.create(JSON.parse(event.body), (err, record) => {
-    if (err) {
-      return {
-        statusCode: error.statusCode || 500,
-        body: String(error),
-      }
-    }
+  const [error, record] = await table.create(
+    JSON.parse(event.body),
+    (error, record) => [error, record]
+  )
+
+  if (err) {
     return {
-      statusCode: 200,
-      body: JSON.stringify(record),
+      statusCode: error.statusCode || 500,
+      body: String(error),
     }
-  })
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(record),
+  }
 }
