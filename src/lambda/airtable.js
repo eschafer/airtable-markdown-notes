150b0d737c7063f9821b8cd35ff147afda5b928d
Elizabeth Schafer <elizabeth.schafer.wenk@gmail.com>
2019-08-17 14:00:48 -0400

    Added missing bracket and fixed formatting

--- src/lambda/airtable.js
+++ src/lambda/airtable.js
@@ -1,21 +1,22 @@
 exports.handler = async (event, context) => {
-  const Airtable = require('airtable');  
+  const Airtable = require('airtable')
   await Airtable.configure({
     endpointUrl: 'https://api.airtable.com',
-    apiKey: process.env.AIRTABLE_API_KEY
-  });
-  const base = await Airtable.base(process.env.BASE_ID);
-  const table = await base('test-table');
+    apiKey: process.env.AIRTABLE_API_KEY,
+  })
+  const base = await Airtable.base(process.env.BASE_ID)
+  const table = await base('test-table')
 
   table.create(JSON.parse(event.body), (err, record) => {
     if (err) {
       return {
         statusCode: error.statusCode || 500,
-        body: String(error)
+        body: String(error),
       }
     }
     return {
       statusCode: 200,
       body: JSON.stringify(record),
     }
-  });
+  })
+}
